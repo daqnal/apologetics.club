54dce8193e10a6e2ad79cb7e400fe47ae5995774
daqnal <dxgd@pm.me>
2025-10-20 12:22:31 -0400

    get basic svelte app up and running with tailwind and daisyui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button class="btn btn-primary">test</button>
